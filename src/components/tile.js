7d9f0718d4a27e18e3df5a50134840422dd28b8b
ddunn2 <dd4dev@gmail.com>
2019-11-08 11:18:46 +0000

    Add stlying to cards

--- src/components/tile.js
+++ src/components/tile.js
@@ -1,4 +1,5 @@
 import React from "react";
+import githubLogo from "../images/header_github_icon.svg";
 
 class Tile extends React.Component {
   constructor(props) {
@@ -11,10 +12,6 @@ class Tile extends React.Component {
       this.desc = this.props.desc;
     }
 
-    this.state = {isToggleOn: true};
-
-    // This binding is necessary to make `this` work in the callback
-    this.handleClick = this.handleClick.bind(this);
     this.copy = this.copy.bind(this);
   }
 
@@ -25,35 +22,24 @@ class Tile extends React.Component {
     document.execCommand("copy");
   }
 
-  handleClick(e) {
-    e.preventDefault();
-    this.setState(state => ({
-      isToggleOn: !state.isToggleOn
-    }));
-  }
   render() {
-    if (this.state.isToggleOn) {
-      return (
-        <div className="tile card text-center">
-          <h5 className="heading-tile">{this.props.heading}</h5>
+    return (
+      <div className="stack-card">
+        <section className="github-header">
+          <p>View in GitHub</p>
+          <img src={ githubLogo } alt="GitHub Logo"></img>
+        </section>
+        <article className="stack-info">
+          <h5>{this.props.heading}</h5>
           <p>{this.desc}</p>
-          <a href="/" onClick={this.handleClick} className="btn btn-primary w-50 mx-auto" role="button">Select</a>
+        </article>
+        <div class="command-input">
+          <p>Some text describing this..</p>
+          <input id="input-cli" type="text" name="cli" value={this.props.cmd} readOnly></input>
+          <i onClick={this.copy} className="far fa-copy"></i>
         </div>
-      );
-    } else {
-      return (
-        <div id={this.props.id} className="tile card flip">
-          <i onClick={this.handleClick} className="fas fa-arrow-left" title="flip card"></i>
-          <h5 id="tile-heading" className="heading-tile">{this.props.heading}</h5>
-
-          <div id="command-input">
-            <input id="input-cli" type="text" name="cli" value={this.props.cmd} readOnly></input>
-            <i onClick={this.copy} className="far fa-copy"></i>
-          </div>
-          <a href={this.props.github} className="btn btn-clear w-75 mx-auto" role="button">View in GitHub</a>
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
